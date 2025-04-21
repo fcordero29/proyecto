ca3c8dfa661cc41350dc8b6a81a08d4c55b29438
Franklin Emmanuel Cordero Bobadilla <80490872+fcordero29@users.noreply.github.com>
2025-04-20 23:27:25 -0400

    update

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    preset: 'jest-puppeteer',
+    testMatch: ['**/test/**/*.test.js'], // ajusta esto si tu carpeta es "test" o "__tests__"
+  };
+  
--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,13 @@ describe('Test Página Hola Mundo', () => {
     await page.goto('http://localhost:8080');
   });
 
-  test('Debe mostrar texto \"Práctica DevOps CI/CD\"', async () => {
-    await expect(page).toMatch('Práctica DevOps CI/CD');
+  test('Debe mostrar el mensaje principal', async () => {
+    const html = await page.content();
+    expect(html).toContain("Bienvenido a la práctica final del curso de DevOps con CI/CD utilizando GitHub.");
+  });
+
+  test('Debe mostrar el título ¡Hola Mundo!', async () => {
+    const html = await page.content();
+    expect(html).toContain("¡Hola Mundo!");
   });
 });
